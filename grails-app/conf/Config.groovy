2a77ec3e5050bc4b21f29990f5f10a32245886c5
Glen Smith <glen@bytecode.com.au>
2009-05-27 19:32:52 +1000

    Upgraded to Grails 1.1.1

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -15,3 +15,7 @@
         password = "password"
     }
     
+
+// The following properties have been added by the Upgrade process...
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
